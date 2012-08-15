8389ad47b56332a9f6d0f4aa96452fa5e672dcd8
Andrew Nordman <cadwallion@gmail.com>
2012-08-15 14:21:26 -0500

    Adjusting bootstrap

--- test/bootstrap.php
+++ test/bootstrap.php
@@ -1,17 +1,12 @@
 <?php
 
-// Configure PHP
-if (!defined('E_DEPRECATED')) {
-    define('E_DEPRECATED', 8192);
+function loader($class)
+{
+    $file = $class . '.php';
+    if (file_exists($file)) {
+        require $file;
+    }
 }
 
-ini_set('error_reporting', E_ALL | E_STRICT | E_DEPRECATED);
-ini_set('display_errors', true);
-
-// Register the autoloader
-spl_autoload_register(
-    function($class){
-        require str_replace(array('_', '\\'), '/', $class) . '.php';
-    }
-);
+spl_autoload_register('loader');
 ?>
